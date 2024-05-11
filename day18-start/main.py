aa15d26250a1e1103bbdbce1408bd6560c07d89e
brocoli05 <mjkimthebest@gmail.com>
2024-05-11 01:55:37 -0400

    add turtle graphics project

--- day18-start/main.py
+++ day18-start/main.py
@@ -45,13 +45,59 @@ my_timmy.color("coral")
 
 
 ########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
-           "wheat", "SlateGray", "SeaGreen"]
-angle = [0, 90, 180, 270, 360]
-my_timmy.width(3)
-for i in range(10):
-    steps = int(random.random() * 100)
-    angle = int(random.choice(angle))
-    my_timmy.color(random.choice(colours))
-    my_timmy.right(angle)
-    my_timmy.forward(steps)
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
+#            "wheat", "SlateGray", "SeaGreen"]
+# directions = [0, 90, 180, 270]
+# my_timmy.width(10)
+# # my_timmy.pensize(10)
+# my_timmy.speed("fastest")
+#
+# # Turtle Graphics uses normalized colours - floating point values from 0.0 to 1.0.
+# # ==> need to tell TG to use 8 bit colours.
+# screen = Screen()
+# screen.colormode(255)
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     color = (r, g, b)
+#     return color
+#
+#
+# for i in range(200):
+#     # my_timmy.color(random.choice(colours))
+#     my_timmy.color(random_color())
+#     my_timmy.forward(30)
+#     my_timmy.setheading(random.choice(directions))
+#
+# # for i in range(10):
+# #     steps = int(random.random() * 100)
+# #     angle = int(random.choice(directions))
+# #     my_timmy.color(random.choice(colours))
+# #     my_timmy.right(angle)
+# #     my_timmy.forward(steps)
+
+
+########### Challenge 5 - Spirograph ########
+screen = Screen()
+screen.colormode(255)
+my_timmy.speed("fastest")
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_spirograph(size_of_gap):
+    for i in range(int(360 / size_of_gap)):
+        my_timmy.color(random_color())
+        my_timmy.circle(100)
+        my_timmy.setheading(my_timmy.heading() + size_of_gap)
+
+
+draw_spirograph(5)  # 5 degrees of each time
+
+screen.exitonclick()
