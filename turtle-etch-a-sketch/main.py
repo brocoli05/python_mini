ef1e2bd3fc57f22eaac589df005cedcae93a51d5
brocoli05 <mjkimthebest@gmail.com>
2024-05-15 23:52:07 -0400

    add etch a sketch game using turtle GUI

---
+++ turtle-etch-a-sketch/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear() # delete all path
+    tim.penup()
+    tim.home()  # back to the center
+    tim.down()
+
+
+screen.listen()
+screen.onkey(move_forwards, "Up")
+# screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
